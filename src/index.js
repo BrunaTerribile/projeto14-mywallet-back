55b77ef05d3f91c672cab0c5b026cb75947e8496
Bruna Terribile <brunaterribile@gmail.com>
2022-11-19 16:11:20 -0300

    Libs + index config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
+import { signIn, signUp } from './controllers/users.controller';
 import dotenv from 'dotenv';
-dotenv.config();
+import joi from 'joi'
+
+const userSchema = joi.object({
+    name: joi.string().required(),
+    password: joi.string().required(),
+    email: joi.string().email().required()
+})
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -14,16 +22,25 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 try {
     await mongoClient.connect();
     console.log("MongoDB conected!");
-}catch (err){
+} catch (err){
     console.log(err);
 }
 
 const db = mongoClient.db("mywalllet");
 const usersCollection = db.collection("users");
 const extractCollection = db.collection("extract");
+const sessionsCollection = db.collection("sessions")
 
 
+app.post("/", signIn);
 
+app.post("/sign-up", signUp);
+
+app.get("/extract", getExtract);
+
+app.post("/new-entry", addEntry);
+
+app.post("/new-outgo", addOutgo);
 
 
 
