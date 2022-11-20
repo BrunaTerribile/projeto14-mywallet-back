bdd570cf4d854e1b0a58b9ce33f3645efc76c74b
Bruna Terribile <brunaterribile@gmail.com>
2022-11-20 20:37:17 -0300

    Feat: addEntry and addOutgo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
-import sessionsCollection from "../db"
+import sessionsCollection from "../db.js"
+import extractCollection from "../db.js"
 
 export async function getExtract(req, res){
     const { authorization } = req.headers;
@@ -10,14 +11,14 @@ export async function getExtract(req, res){
 
     try {
         const session = await sessionsCollection.findOne({token})
-        const user = await usersCollection.findOne({_id: session?.userId})
+        const extract = await extractCollection.findOne({_id: session?.userId})
 
-        if(!user){
+        if(!extract){
             return res.sendStatus(401)
         }
         
         delete user.password;
-        res.send(user)
+        res.send(extract)
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
@@ -25,8 +26,63 @@ export async function getExtract(req, res){
 }
 
 export async function addEntry(req, res){
+    const entry = req.body; //date, description and value
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const session = await sessionsCollection.findOne({ token });
+        const user = await usersCollection.findOne({ _id: session?.userId });
+
+        if (!user) {
+        return res.sendStatus(401);
+        }
+
+        await extractCollection.insertOne({
+        ... entry,
+        userId: user._id,
+        isIncoming: true
+        });
+
+        res.sendStatus(201);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
 
 export async function addOutgo(req, res){
+    const outgo = req.body; //date, description and value
+    const { authorization } = req.headers;
 
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const session = await sessionsCollection.findOne({ token });
+        const user = await usersCollection.findOne({ _id: session?.userId });
+
+        if (!user) {
+        return res.sendStatus(401);
+        }
+
+        await extractCollection.insertOne({
+        ... outgo,
+        userId: user._id,
+        isIncoming: false
+        });
+
+        res.sendStatus(201);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
